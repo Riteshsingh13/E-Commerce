c03c9e179b6e1f11875fe9287270eb6fd43d02e9
Ritesh <13singhritesh@gmail.com>
2024-03-22 14:00:05 +0530

    Implemented Nativation bar

---
+++ src/Customer/Components/HomeCarousel/mainCarouselData.js
@@ -0,0 +1,27 @@
+export const mainCarouselData = [
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
+    path: "women/clothing/women_saree",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_2.jpg",
+    path: "women/clothing/women_saree",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+    path: "women/clothing/women_saree",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_7.jpg",
+    path: "women/clothing/women_saree",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_1.jpg",
+    path: "women/clothing/women_saree",
+  },
+];
---
+++ src/Customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainCarousel from "../../Components/HomeCarousel/MainCarousel";
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarousel />
+      
+    </div>
+  );
+};
+
+export default HomePage;
